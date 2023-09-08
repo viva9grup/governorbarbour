334ae9a6b45b45bafacac3dff5cfe5561bd6ae2d
Viva99 <100474729+viva9grup@users.noreply.github.com>
2023-09-08 13:46:11 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,2 @@
-importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
-AMP_SW.init({
-assetCachingOptions: [{
-regexp: /\.(png|jpg|webp|svg)/,
-cachingStrategy: 'CACHE_FIRST'
-}],
-offlinePageOptions: {
-url: 'https://www.governorbarbour.com/offline.html',
-assets: []
-}
-});
+self.addEventListener('install',function(event){var offlineRequest=new Request('index.html');event.waitUntil(fetch(offlineRequest).then(function(response){return caches.open('offline').then(function(cache){console.log('[oninstall] Cached offline page',response.url);return cache.put(offlineRequest,response);});}));});self.addEventListener('fetch',function(event){var request=event.request;if(request.method==='GET'){event.respondWith(fetch(request).catch(function(error){console.error('[onfetch] Failed. Serving cached offline fallback '+
+error);return caches.open('offline').then(function(cache){return cache.match('index.html');});}));}});
